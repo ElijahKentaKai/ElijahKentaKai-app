97ba3a537b2d5a329bbcf3d2a00d53673440761c
kenta918 <56744380+kenta918@users.noreply.github.com>
2020-04-28 20:28:11 -1000

    Add files via upload

--- tetris.js
+++ tetris.js
@@ -5,22 +5,49 @@ function cube(){
   this.y= 20;
   this.w=69;
   this.h=69;
-  this.gravity = 0.1;
-  this.velocity = 0;
-  
+  this.history=[];
+  this.update  = function(){
+    
+    if(this.y < 640){
+      this.y += 34.5;
+    }
+    else if((this.y ==  640) ||(this.y > 640  )){{
+      
+      
+      var v = createVector(this.x,this.y);
+      this.history.push(v)
+      this.x =688;
+      this.y=20;
+      
+    }
+    }
+  }
   this.show = function(){
     fill(255,0,0);
     rect(this.x,this.y,this.w,this.h)
     
-  }
-  this.update  = function(){
-    if(this.y < 621){
-      this.y += 34.5;
-    }
-    else{
-      return;
-    }
-  }
     
+  }
+  
+    
+  
+}
+
+
+function lShape(){
+  this.x = 688;
+  this.y2= 20;
+  this.gravity = 0.1;
+    
+  
+  
+  this.show2 = function(){
+    fill(0,255,0);
+    beginShape()
+    vertex(10,10);
+    vertex(34.5,34.5)
+    closeShape()
+  }
+  
   
 }
